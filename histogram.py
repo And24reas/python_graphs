3a4796b9d4e337ff215ff38f1a180e4615ba6427
andreas <meinakkount@gmail.com>
2021-09-29 22:25:45 +0200

    added json files

--- histogram.py
+++ histogram.py
@@ -1,7 +1,11 @@
 import numpy
 import matplotlib.pyplot as plt
-x = numpy.random.uniform(0.0, 5.0, 250)
+x = numpy.random.uniform(0.0, 10.0, 50)
+y = numpy.random.uniform(0.0, 10.0, 50)
+z = numpy.random.uniform(0.0, 10.0, 50)
 
-plt.hist(x,5)
+
+
+plt.scatter(x,y)
 plt.show()
 print(x)
